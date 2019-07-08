bdf02c0c76958d62804efa040819dba6477d3265
EddieSCJ <EddieStudyIfs@gmail.com>
2019-07-07 23:11:57 -0300

    Board and piece - Created

---
+++ src/boardgame/Board.java
@@ -0,0 +1,31 @@
+package boardgame;
+
+public class Board {
+
+	private int rows;
+	private int colunms;
+	private Piece pieces[][];
+	
+	public Board(int rows, int columns) {
+		this.setColunms(columns);
+		this.setRows(rows);
+		pieces = new Piece[columns][rows];
+	}
+	
+	public int getRows() {
+		return rows;
+	}
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+	public int getColunms() {
+		return colunms;
+	}
+	public void setColunms(int colunms) {
+		this.colunms = colunms;
+	}
+	
+	
+	
+	
+}
