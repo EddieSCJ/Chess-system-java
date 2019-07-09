aff0e72b70f5b5db5e35d099f8056a3ab02023eb
EddieSCJ <EddieStudyIfs@gmail.com>
2019-07-09 03:32:53 -0300

    Added the possible moves of king

--- Chess/chess/pieces/King.java
+++ Chess/chess/pieces/King.java
@@ -1,6 +1,8 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
+import boardgame.exceptions.BoardException;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -16,10 +18,66 @@ public class King extends ChessPiece {
 	}
 
 	@Override
-	public boolean[][] possibleMoves() {
+	public boolean[][] possibleMoves() throws BoardException {
 		// TODO Auto-generated method stub
 		
 		boolean possibleMoves[][] = new boolean[getBoard().getRowsQuantity()][getBoard().getColunmsQuantity()];
+		
+		Position position = new Position(0, 0);
+		
+		//above 
+		
+		position.setValues(this.position.getRow()-1, this.position.getColumn());
+		if(getBoard().positionExists(position) && (!getBoard().thereIsAPiece(position) || isThereAnOpponentPiece(position))) {
+			possibleMoves[position.getRow()][position.getColumn()]=true;
+		}
+		
+		//right diagonal below
+		
+		position.setValues(this.position.getRow()-1, this.position.getColumn()+1);
+		if(getBoard().positionExists(position) && (!getBoard().thereIsAPiece(position) || isThereAnOpponentPiece(position))) {
+			possibleMoves[position.getRow()][position.getColumn()]=true;
+		}
+		
+		//right diagonal above
+		
+		position.setValues(this.position.getRow()+1, this.position.getColumn()+1);
+		if(getBoard().positionExists(position) && (!getBoard().thereIsAPiece(position) || isThereAnOpponentPiece(position))) {
+			possibleMoves[position.getRow()][position.getColumn()]=true;
+		}
+				
+		//left diagonal above
+		
+		position.setValues(this.position.getRow()+1, this.position.getColumn()-1);
+		if(getBoard().positionExists(position) && (!getBoard().thereIsAPiece(position) || isThereAnOpponentPiece(position))) {
+			possibleMoves[position.getRow()][position.getColumn()]=true;
+		}		
+
+		//left diagonal below
+		
+		position.setValues(this.position.getRow()-1, this.position.getColumn()-1);
+		if(getBoard().positionExists(position) && (!getBoard().thereIsAPiece(position) || isThereAnOpponentPiece(position))) {
+			possibleMoves[position.getRow()][position.getColumn()]=true;
+		}		
+		
+		
+		//below
+		position.setValues(this.position.getRow()+1, this.position.getColumn());
+		if(getBoard().positionExists(position) && (!getBoard().thereIsAPiece(position) || isThereAnOpponentPiece(position))) {
+			possibleMoves[position.getRow()][position.getColumn()]=true;
+		}
+		//right
+		position.setValues(this.position.getRow(), this.position.getColumn()+1);
+		if(getBoard().positionExists(position) && (!getBoard().thereIsAPiece(position) || isThereAnOpponentPiece(position))) {
+			possibleMoves[position.getRow()][position.getColumn()]=true;
+		}
+		
+		//left
+		position.setValues(this.position.getRow(), this.position.getColumn()-1);
+		if(getBoard().positionExists(position) && (!getBoard().thereIsAPiece(position) || isThereAnOpponentPiece(position))) {
+			possibleMoves[position.getRow()][position.getColumn()]=true;
+		}
+		
 		
 		return possibleMoves;
 	}
